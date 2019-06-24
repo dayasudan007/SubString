98f15c62a7c086be51cc82fcde956be900397066
dayasudan007 <52152452+dayasudan007@users.noreply.github.com>
2019-06-24 13:06:32 +0530

    Update and rename Main.java to Program.java

---
+++ Program.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Program
+{
+	final static int no_of_chars = 256;
+	static int maxDistinctChar(String str,int n)
+	{
+	    int count[] = new int[no_of_chars];
+	    for(int i=0;i<n;i++)
+	    {
+	        count[str.charAt(i)]++;
+	    }
+	    int max=0;
+	    for(int i=0;i<no_of_chars;i++)
+	    {
+	        if(count[i]!=0)
+	        {
+	            max++;
+	        }
+	    }
+	    return max;
+	}
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		String a = s.nextLine();
+		//System.out.println(a);
+		int len = maxDistinctChar(a,a.length());
+		System.out.println(len);
+	}
+}
